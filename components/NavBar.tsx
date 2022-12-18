cc089a82658f6bedb4227ce53749cfc24655be8f
ben-yeung <benjaminyeung@berkeley.edu>
2022-12-17 19:40:04 -0800

    Added ssmy site link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,9 @@ import ssmyLogo from "./assets/SSMYWhite.png";
 const NavBar = () => {
   return (
     <nav className="bg-zinc-900 p-4 flex justify-between min-w-max shadow-lg">
-      <img className="w-10 object-contain ml-6" src={"/assets/SSMYWhite.png"} />
+      <a href="https://ssmytech.com" className="flex items-center" target={"_blank"}>
+        <img className="w-10 object-contain ml-6" src={"/assets/SSMYWhite.png"} />
+      </a>
       <ul className="flex justify-end items-center px-5 space-x-6 font-medium text-lg">
         <li>
           <button className="px-3 py-2 rounded-lg hover:bg-zinc-800">Home</button>
